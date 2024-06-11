053b493b0721f89d8307708b99fd35d0b952b6f3
Ricardo Tagliaverga tagliaverga@icloud.com <tagliaverga@icloud.com>
2024-06-11 17:31:25 -0300

    show de itinerary

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,6 @@
 class TripsController < ApplicationController
-  before_action :set_trip, only:  %i[show edit update destroy]
-  before_action :set_itinerary, only:  %i[show edit update destroy]
+  before_action :set_trip, only: %i[show edit update destroy]
+  before_action :set_itinerary, only: %i[edit update destroy]
 
   def new
     @trip = Trip.new
