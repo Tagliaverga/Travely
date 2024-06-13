e543461a693c19c8312935157cf318e8f0db56dc
Ricardo Tagliaverga tagliaverga@icloud.com <tagliaverga@icloud.com>
2024-06-13 10:58:53 -0300

    services controler and model created

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,16 @@
+class ServicesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_11_142331) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_13_135534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_11_142331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["trip_id"], name: "index_itineraries_on_trip_id"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "trips", force: :cascade do |t|
