3a481680229099ea9c029053be382045e58b56e5
Ricardo Tagliaverga tagliaverga@icloud.com <tagliaverga@icloud.com>
2024-06-18 18:32:44 -0300

    chat

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,10 @@ Rails.application.routes.draw do
 
   resources :documents, only: %i[destroy show]
 
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
+
   root to: "pages#home"
 
   get "up" => "rails/health#show", as: :rails_health_check
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_18_145024) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_200001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_18_145024) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "documents", force: :cascade do |t|
     t.float "price"
     t.string "description"
@@ -69,6 +75,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_18_145024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["trip_id"], name: "index_itineraries_on_trip_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "services", force: :cascade do |t|
@@ -106,6 +122,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_18_145024) do
     t.string "city"
     t.boolean "contractor", default: false
     t.boolean "admin"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -117,6 +134,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_18_145024) do
   add_foreign_key "experiences", "services"
   add_foreign_key "experiences", "users"
   add_foreign_key "itineraries", "trips"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "services", "users"
   add_foreign_key "trips", "users"
 end
